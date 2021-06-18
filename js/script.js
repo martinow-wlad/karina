cc656cd11d6cbd20f13ecf764c0f21bc364f6a39
Vladymyr <vladymyr.martynov@gmail.com>
2021-06-18 23:19:08 +0300

    update

--- js/script.js
+++ js/script.js
@@ -1,22 +1,18 @@
 document.addEventListener('DOMContentLoaded', function () {
-        let videoClick = document.querySelector('.play-icon');
-        let video = document.querySelector('.video video');
-        let line = document.querySelector('.line');
-        videoClick.addEventListener('click', function (e) {
-            e.preventDefault();
-            videoClick.classList.add('hidden');
-            video.play();
-        });
-
-        let slider = document.querySelector('.rewiee');
-
-        let flkty = new Flickity(slider, {
-            // options
-            // cellAlign: 'left',
-            // contain: true,
-            pageDots: false,
-            // groupCells: 2,
-            wrapAround: true
-        });
-
+    let videoClick = document.querySelector('.play-icon');
+    let video = document.querySelector('.video video');
+    videoClick.addEventListener('click', function (e) {
+        e.preventDefault();
+        videoClick.classList.add('hidden');
+        video.play();
     });
+    $(function () {
+        $(".form_input_tel").mask("+380(99)-999-99-99");
+    });
+    $(document).ready(function () {
+        $('.burger-menu').click(function (event){
+            $('.burger-menu,.menu').toggleClass('active');
+            $('body').toggleClass('lock');
+        });
+    });
+});
