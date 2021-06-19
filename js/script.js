37c14e408c10a8286f05b42312ada8227b021d24
Vladymyr <vladymyr.martynov@gmail.com>
2021-06-19 14:24:52 +0300

    update

--- js/script.js
+++ js/script.js
@@ -1,11 +1,14 @@
 document.addEventListener('DOMContentLoaded', function () {
     let videoClick = document.querySelector('.play-icon');
     let video = document.querySelector('.video video');
-    videoClick.addEventListener('click', function (e) {
-        e.preventDefault();
-        videoClick.classList.add('hidden');
-        video.play();
-    });
+    if (videoClick) {
+        videoClick.addEventListener('click', function (e) {
+            e.preventDefault();
+            videoClick.classList.add('hidden');
+            video.play();
+        });
+    }
+   
     $(function () {
         $(".form_input_tel").mask("+380(99)-999-99-99");
     });
